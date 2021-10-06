fc2946fa91802b4a34e8c9f3518d5ec5207c5087
restureese <restureese@gmail.com>
2021-10-06 16:30:23 +0700

    rabbitmq > queue

---
+++ rabbitmq/queue/publisher.py
@@ -0,0 +1,38 @@
+"""
+    @Author varx
+    @Date 06/10/21 15.33
+    @Describe 
+"""
+import sys
+import asyncio
+from aio_pika import connect, Message, DeliveryMode
+
+
+async def main(loop):
+    # Perform connection
+    connection = await connect("amqp://guest:guest@localhost/", loop=loop)
+
+    # Creating a channel
+    channel = await connection.channel()
+
+    message_body = b" ".join(
+        arg.encode() for arg in sys.argv[1:]) or b"Hello World!"
+
+    message = Message(
+        message_body,
+        delivery_mode=DeliveryMode.PERSISTENT
+    )
+
+    # Sending the message
+    await channel.default_exchange.publish(
+        message, routing_key="task_queue"
+    )
+
+    print(" [x] Sent %r" % message)
+
+    await connection.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
